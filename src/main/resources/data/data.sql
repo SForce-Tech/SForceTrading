a746b1bba1f94332b566fe27343ada3b8826da32
Adrian Flores <adrian@sforce.tech>
2024-06-10 13:37:34 -0400

    updated data.sql script to load some users.

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -1,3 +1,12 @@
--- data.sql
-INSERT INTO users (email, name) VALUES ('user1@example.com', 'User One');
-INSERT INTO users (email, name) VALUES ('user2@example.com', 'User Two');
+INSERT INTO users (
+    id, address, address2, city, country, email, first_name, last_name, password, phone, state, username, zip_code
+) VALUES
+    (1, '9731 Fontainebleau Blvd, Apt 112', 'Apt 112', 'Miami', 'United States', 'jim.doe@example.com', 'Jimy', 'Doe', '$2a$10$Wh7pFnjWUMSIXu.XuRN9p.4aZCvIy9zRrUnu.y42AvC/HNqXkc9wu', '7867647670', 'FL', 'jimdoe', '33172'),
+    (2, '123 Main St', 'Apt 4', 'Springfield', 'USA', 'john.doe@example.com', 'John', 'Doe', '$2a$10$dCEBLV80.S7gkVqFB6EWkudNxEX.RufP21VlYleAHjSNNQ4KGG3AS', '1234567890', 'IL', 'johndoe', '62701'),
+    (9, '123 Main St', 'Apt 4', 'Springfield', 'USA', 'victoria.doe@example.com', 'Victoria', 'Doe', '$2a$10$LYMpWijv0FKiJsj2s6yTHOgExfVRTk2becvICiEut1WesETZTHzwK', '1234567890', 'IL', 'victoriadoe', '62701'),
+    (10, '123 Main St', 'Apt 4', 'Springfield', 'USA', 'eri.doe@example.com', 'Eri', 'Doe', '$2a$10$rKoxV2UBLrOsryBku9DhuudMQ2aJYc074ypSSik3.747lOctxs3Zm', '1234567890', 'IL', 'eridoe', '62701'),
+    (11, '123 Main St', 'Apt 4', 'Springfield', 'USA', 'arian.doe@example.com', 'Arian', 'Doe', '$2a$10$sDf0VTcuE6qMElAM9OOfueyJbXF7eVEbmbXtkglob/kePpzCB.bYW', '1234567890', 'IL', 'ariandoe', '62701'),
+    (12, '123 Main St', 'Apt 4', 'Springfield', 'USA', 'adrian.doe@example.com', 'Adrian', 'Doe', '$2a$10$DI62rwF5hRzwJ/thiBqxBeMasTFMj5trk8ijg4nlANx11y0W8U6na', '1234567890', 'IL', 'adrian.doe', '62701'),
+    (13, '123 Main St', 'Apt 4', 'Springfield', 'USA', 'leo.doe@example.com', 'Leon', 'Doe', '$2a$10$kLYBz6MOeKVfvybnsAzy4uttAwl5f1dA03KnYRyK.LRscAb6ctF3y', '1234567890', 'IL', 'leodoe', '62701'),
+    (20, '123 Main St', 'Apt 4', 'Springfield', 'USA', 'joan.doe@example.com', 'Joan', 'Doe', '$2a$10$UAlAEuCGBoE2KEc2Orb/AOxKUsJglv56lPEmgsVH8qMjDVOuKEhdi', '1234567890', 'IL', 'joandoe', '62701'),
+    (21, '9731 Fontainebleau Blvd, Apt 112', 'Apt 112', 'Miami', 'United States', 'nellytmedinav@hotmail.com', 'Nelly', 'Medina', '$2a$10$3uDW0mxzXwDodAMP0N9zKeQINyU1Kn.5NWRc9tTYrSjTlnhW3QbQ2', '7867647670', 'FL', 'nelly.medina', '33172');
